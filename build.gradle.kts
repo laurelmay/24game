d726671e85d08cfc6253f6239e0353fd93c0ffbf
Laurel May <laurel@laurelmay.me>
2025-09-27 15:58:00 -0400

    chore: update to Spring Boot 4.0.0-M3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
 plugins {
 	java
-	id("org.springframework.boot") version "4.0.0-M2"
+	id("org.springframework.boot") version "4.0.0-M3"
 	id("io.spring.dependency-management") version "1.1.7"
-	id("org.graalvm.buildtools.native") version "0.10.6"
 }
 
 group = "me.laurelmay"
